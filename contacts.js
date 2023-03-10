130618049f6a4a602f5a720f379d4c4828ad863a
Viktoriia181818 <vikulyashaaa@gmail.com>
2023-03-10 22:32:25 +0200

    FINAL

--- contacts.js
+++ contacts.js
@@ -1,2 +1,46 @@
 const fs = require("fs").promises;
+const path = require("path");
+const { v4 } = require("uuid");
 
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contactParse = JSON.parse(data);
+  return contactParse;
+}
+
+async function getContactById(contactId) {
+  const contactParse = await listContacts();
+  const result = contactParse.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(contactId) {
+  const contactParse = await listContacts();
+  const idx = contactParse.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const newContacts = contactParse.filter((_, index) => index !== idx);
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+  return contactParse[idx];
+}
+
+async function addContact(name, email, phone) {
+  const contactParse = await listContacts();
+  const newProduct = { name, email, phone, id: v4() };
+  contactParse.push(newProduct);
+  await fs.writeFile(contactsPath, JSON.stringify(contactParse));
+  return newProduct;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-const operation = require("./contacts");
+const contactOperations = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const products = await contactOperations.listContacts();
+      console.table(products);
+      break;
+
+    case "get":
+      const product = await contactOperations.getContactById(id);
+      if (!product) {
+        throw new Error(`Product with id=${id} not found`);
+      }
+      console.log(product);
+      break;
+
+    case "add":
+      const newProduct = await contactOperations.addContact(name, email, phone);
+      console.log(newProduct);
+      break;
+
+    case "remove":
+      const removeProduct = await contactOperations.removeContact(id);
+      console.log(removeProduct);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
